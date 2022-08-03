34ecf47bc22b79182a5e4028febb077f17ff9c02
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-08-03 11:30:41 +0200

    fix

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+import random
+
+os.system("git checkout hamzaplojovic-patch-1")
+with open("test.txt", "w") as f:
+    f.write(str(random.randint(0, 10000000)))
+os.system("git add .")
+os.system("git commit -m 'fix'")
+os.system("git push")
+os.system("git checkout master")
